79a58ffaa700b4d0615567ea32f533482805885a
Regina Steffen <reginasteffen@live.com>
2024-10-18 09:43:23 -0500

    imported flask, sqlalchemy; set up app

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 """Flask app for Cupcakes"""
+from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+from models import 
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "secretkey"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+
+connect_db(app)
