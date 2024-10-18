da5f856aa9ea46e69a0984b9dfff994293af5171
Regina Steffen <reginasteffen@live.com>
2024-10-18 16:29:40 -0500

    created route returning JSON data for all cupcakes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """Flask app for Cupcakes"""
-from flask import Flask
+from flask import Flask, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Cupcake
 
@@ -13,3 +13,12 @@ debug = DebugToolbarExtension(app)
 
 
 connect_db(app)
+
+@app.route("/api/cupcakes")
+def list_cupcakes():
+    """get data about all cupcakes"""
+    cupcakes = [cupcake.serialize_cupcake() for cupcake in Cupcake.query.all()]
+
+    return jsonify(cupcakes=cupcakes)
+
+
